8a83fa05732effc73ee029869ffbc68bb64cd5f3
alexander <alex.shklyarenko22@gmail.com>
2022-11-19 15:47:49 +0200

    черги+апи

---
+++ app/Jobs/FirstEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FirstEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    public $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+       echo $this->message . PHP_EOL;
+    }
+}
